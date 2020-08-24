e222313e0f4fdab151e2460ba4efe31397f6402c
Dario Campagna <dario@localhost.localdomain>
2020-08-24 09:25:14 -0300

    Criando classe de serviço TimeServico.

---
+++ src/main/java/br/com/cbf/campeonatobrasileiro/service/TimeServico.java
@@ -0,0 +1,24 @@
+package br.com.cbf.campeonatobrasileiro.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cbf.campeonatobrasileiro.entity.Time;
+import br.com.cbf.campeonatobrasileiro.repository.TimeRepository;
+
+@Service
+public class TimeServico {
+	
+	@Autowired
+	private TimeRepository timeRepository;
+	
+	public void cadastrarTime(Time time) {
+		timeRepository.save(time);
+	}
+	
+	public List<Time> listar(){
+		return timeRepository.findAll();
+	}
+}
