2483b7ae1092464e3c69041dd311eb1a6a1b4ff6
Dario Campagna <dario@localhost.localdomain>
2020-08-24 09:14:55 -0300

    Criando os repositórios, para acessar o banco de dados.

---
+++ src/main/java/br/com/cbf/campeonatobrasileiro/repository/JogoRepository.java
@@ -0,0 +1,9 @@
+package br.com.cbf.campeonatobrasileiro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.cbf.campeonatobrasileiro.entity.Jogo;
+
+public interface JogoRepository extends JpaRepository<Jogo, Integer>{
+
+}
